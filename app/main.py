b35724749e7a7dacab420aa5bea6995c22a93fd2
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-06-16 00:08:58 +0500

    create webhook

--- app/main.py
+++ app/main.py
@@ -6,8 +6,21 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.core import load_config
+from app.bot import dp, bot
 
 
 settings = load_config()
 
-fastapi_app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # dp.include_router()
+    webhook_url = settings.api.get_webhook_url
+    await bot.set_webhook(url=webhook_url,
+                          allowed_updates=dp.resolve_used_update_types(),
+                          drop_pending_updates=True,
+                          secret_token=settings.api.secret_webhook)
+    yield
+    await bot.delete_webhook()
+
+
+fastapi_app = FastAPI(lifespan=lifespan)
