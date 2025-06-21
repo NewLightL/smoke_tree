77b7840a91ab0008aff65c71813636d97c248cb5
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-06-21 18:17:48 +0500

    edit base bot

--- app/bot/handlers/base.py
+++ app/bot/handlers/base.py
@@ -1,11 +1,13 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
 
+from app.bot.fonts.message_font import MessageFont
+from app.bot.keyboars.base_keyboards import start_keyboard
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_message(mess: Message):
-    await mess.answer(f"Привет, {mess.from_user.full_name}")
+    await mess.answer(MessageFont.start, reply_markup=start_keyboard)
