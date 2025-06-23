987834dcbf3c927774113789b2c5e45cdc95b017
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-06-24 01:05:58 +0500

    create callback fabric

---
+++ app/bot/callback/catalog_fabric.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+from aiogram.filters.callback_data import CallbackData
+
+
+class Action(str, Enum):
+    select = "select"
+    apply = "apply"
+    reset = "reset"
+    paginate = "paginate"
+
+
+class CallbackProduct(CallbackData, prefix="products"):
+    action: Action
+    filter_type: str|None = None  # "taste", "brand" и т.д.
+    value: str|int|float|bool|None = None  # выбранное значение
+    page: int = 0  # текущая страница
