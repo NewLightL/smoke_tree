f3794887e4dfad486f616b5cdf86eb107f26442b
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-06-27 20:27:52 +0500

    update state

--- app/bot/state/catalog_state.py
+++ app/bot/state/catalog_state.py
@@ -4,5 +4,6 @@ from aiogram.fsm.state import StatesGroup, State
 class ViewCatalog(StatesGroup):
     view_introductory_page = State()
     view_filters = State()
+    view_filter_parametr = State()
     view_items = State()
     card_item = State()
