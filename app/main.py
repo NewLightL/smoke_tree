d0646f0206c84cecd122f67b8a8a1c90911ff502
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-07-05 00:38:30 +0500

    add static

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ poetry run uvicorn app.main:fastapi_app
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.core import load_config
-from app.bot import dp, bot, base_router, catalog_router
+from app.bot import dp, bot, base_router, catalog_router, search_router
 from app.api import webhook_router
 
 
@@ -14,19 +15,20 @@ settings = load_config()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    dp.include_router(base_router)
     dp.include_router(catalog_router)
+    dp.include_router(search_router)
+    dp.include_router(base_router)
     webhook_url = settings.api.get_webhook_url
     await bot.set_webhook(url=webhook_url,
                           allowed_updates=dp.resolve_used_update_types(),
                           drop_pending_updates=True,
                           secret_token=settings.api.secret_webhook)
-    print("Webhook is ready!!!")
     yield
     await bot.delete_webhook()
-    print("Webhook is closed")
 
 
 fastapi_app = FastAPI(lifespan=lifespan)
+
+fastapi_app.mount(r"/static", StaticFiles(directory=r"app/static"), "static")
 
 fastapi_app.include_router(webhook_router)
