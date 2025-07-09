20c4fde855c6253995a70d76beb55d28d34b9a35
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-07-09 19:38:19 +0500

    update state

--- app/bot/state/catalog_state.py
+++ app/bot/state/catalog_state.py
@@ -5,6 +5,7 @@ class ViewCatalog(StatesGroup):
     view_introductory_page = State()
     view_filters = State()
     view_filter_parametr = State()
+    search_by_name = State()
     select_price = State()
     view_items = State()
     card_item = State()
