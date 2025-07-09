586e2f3bfa6d428d625119c402a9bff414c08608
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-07-10 01:19:35 +0500

    connect pg_trgm

--- app/main.py
+++ app/main.py
@@ -5,16 +5,21 @@ poetry run uvicorn app.main:fastapi_app
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from sqlalchemy import text
 
 from app.core import load_config
 from app.bot import dp, bot, base_router, catalog_router, search_router
 from app.api import webhook_router
+from app.db.helper import helper
 
 
 settings = load_config()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    async with helper.engine.begin() as sess:
+        await sess.execute(text("CREATE EXTENSION IF NOT EXISTS pg_trgm"))
+        await sess.commit()
     dp.include_router(catalog_router)
     dp.include_router(search_router)
     dp.include_router(base_router)
