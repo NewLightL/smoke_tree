d86772b31fb13173cfa4d537daecd107c321a88a
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-08-15 00:29:17 +0300

    edit __init__ for admin

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,9 +2,7 @@ __all__ = (
     "ProductsView",
     "UsersView",
     "AuthAdmin",
-    "OrderViews"
 )
 
 from app.admin.layout import ProductsView, UsersView
-from app.admin.baseview import OrderViews
 from app.admin.auth import AuthAdmin
