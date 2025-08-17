163cf7004f47566f7287b413a91c1c2123d6ac68
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-08-17 20:39:49 +0300

    update call font

--- app/bot/fonts/call_font.py
+++ app/bot/fonts/call_font.py
@@ -1,3 +1,8 @@
 class CallAnswerFont:
     apply = "Поиск товара"
     reset = "Фильтры сброшены"
+    product_select = "Товар добавлен в корзину"
+    product_remove = "Товар убран из корзины"
+    basket_reset = "Крозина очищена"
+    plus_product = "+1 в корзину"
+    minus_product = "-1 в корзину"
