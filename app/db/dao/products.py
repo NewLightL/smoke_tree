093675a4c3bb3e95cd9457d0d33d41e5f13c4488
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-08-17 20:39:00 +0300

    add new func in products dao

--- app/db/dao/products.py
+++ app/db/dao/products.py
@@ -11,6 +11,10 @@ class ProductsDAO(BaseDAO):
     @classmethod
     async def select_products_by_filter(cls, **filter_by: Any):  # type: ignore
         price_in_filter = "price" in filter_by.keys()
+        basket_in_filter = "basket" in filter_by.keys()
+
+        if basket_in_filter:
+            del filter_by["basket"]
 
         async with cls.conn.sessionf() as sess:
             if not filter_by:
@@ -24,7 +28,6 @@ class ProductsDAO(BaseDAO):
             else:
                 query = select(Products).filter_by(**filter_by)
 
-            # print(query.compile(compile_kwargs={"literal_binds": True}))
             query = query.filter(Products.amount > 0)
             res = await sess.execute(query)
             answer = res.scalars().all()
@@ -57,3 +60,15 @@ class ProductsDAO(BaseDAO):
         async with cls.conn.sessionf() as sess:
             res = await sess.execute(query)
             return res.scalars().all()
+
+
+    @classmethod
+    async def get_all_by_id(cls, ids_list: list[int]):
+        stmt = (select(Products)
+               .where(
+                   Products.id.in_(ids_list)
+                ))
+        async with cls.conn.sessionf() as sess:
+            result = await sess.execute(stmt)
+            products = result.scalars().all()
+            return products if products else None
