760f1bb607b639e78bcb0d15ef7162da58d77dc6
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-08-18 03:22:27 +0300

    connect redis with dp

--- app/bot/main.py
+++ app/bot/main.py
@@ -1,8 +1,9 @@
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from app.core import load_config
+from app.core import load_config, redis_backend
 
 
 settings = load_config()
@@ -10,4 +11,6 @@ settings = load_config()
 
 bot = Bot(token=settings.bot.bot_token,
           default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher()
+
+storage = RedisStorage(redis=redis_backend)
+dp = Dispatcher(storage=storage)
