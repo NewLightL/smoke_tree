5c6ee401efa7673a81267b0b7f57d9ec15efc459
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-09-01 21:23:55 +0300

    add func create the card products in basket

--- app/bot/utils/basket.py
+++ app/bot/utils/basket.py
@@ -1,4 +1,5 @@
-from app.bot.fonts.message_font import BasketFont
+from app.bot.fonts.message_font import BasketFont, SearchFont
+from app.bot.utils.catalog import CatalogUtils
 from app.db import Products, ProductsDAO
 
 
@@ -21,3 +22,25 @@ class BasketUtils:
             products_text += f"{brand} {name}, {taste} - {count}шт\n"
 
         return BasketFont.card_basket.format(products_text, total_cost)
+
+    @classmethod
+    def get_card_products_in_basket(cls, product: Products|None, count: int = 1):
+        if product is None:
+            text = BasketFont.basket_is_empty
+        else:
+            text = BasketFont.card_products_in_basket.format(
+                product.name,
+                product.brand,
+                product.taste,
+                product.volume,
+                product.fortress,
+                product.type_nicotine,
+                CatalogUtils.translate_bool(product.chill),
+                (SearchFont.last_products if
+                 product.amount <= 3 else
+                 SearchFont.more_products),
+                product.amount,
+                product.price,
+                count)
+
+        return text
