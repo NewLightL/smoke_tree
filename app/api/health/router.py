08ae89a1c15cae922af179b8ee2b1f019b939e56
Lev Nokhrin <lev.hhh.nohrin@gmail.com>
2025-09-09 23:49:30 +0300

    edit router health

--- app/api/health/router.py
+++ app/api/health/router.py
@@ -4,7 +4,13 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/health")
 
 
+@router.head("/")
+async def head_health_check():
+    """Простой endpoint для проверки работы"""
+    return {"status": "ok", "message": "Server is alive"}
+
+
 @router.get("/")
-async def health_check():
+async def get_health_check():
     """Простой endpoint для проверки работы"""
     return {"status": "ok", "message": "Server is alive"}
